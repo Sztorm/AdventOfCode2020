29619b84baf8cf2d05d23ff633caaba0f8e2cfcb
Sztorm <marcinwicher99@gmail.com>
2020-12-03 11:41:09 +0100

    Updated Day3 with solution for second part of the puzzle. Day3 is completed. Added sample input for day 3 and previous days for completeness.

--- AdventOfCode2020/Day3/Day3.cs
+++ AdventOfCode2020/Day3/Day3.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020
@@ -13,11 +14,10 @@ namespace AdventOfCode2020
             "Day3/output2.txt",
         };
 
-        private static void RunPart1(char[][] inputValues)
+        private static int CountTreesOnSlope(char[][] map, (int row, int column) displacement)
         {
-            int rows = inputValues.Length;
-            int columns = rows > 0 ? inputValues[0].Length : 0;
-            (int row, int column) displacement = (1, 3);
+            int rows = map.Length;
+            int columns = rows > 0 ? map[0].Length : 0;
             (int row, int column) currentPosition = (0, 0);
             int treeCount = 0;
 
@@ -26,17 +26,36 @@ namespace AdventOfCode2020
                 currentPosition = (
                     currentPosition.row + displacement.row,
                     (currentPosition.column + displacement.column) % columns);
-                if (inputValues[currentPosition.row][currentPosition.column] == '#')
+                if (map[currentPosition.row][currentPosition.column] == '#')
                 {
                     treeCount++;
                 }
             }
+            return treeCount;
+        }
+
+        private static void RunPart1(char[][] inputValues)
+        {
+            int treeCount = CountTreesOnSlope(inputValues, (1, 3));
+
             PuzzleIOManager.SaveTextLines(
                 RelativeOutputPaths[0], new string[] { treeCount.ToString() });
         }
 
         private static void RunPart2(char[][] inputValues)
         {
+            Span<int> treeCounts = stackalloc int[]
+            {
+                CountTreesOnSlope(inputValues, (1, 1)),
+                CountTreesOnSlope(inputValues, (1, 3)),
+                CountTreesOnSlope(inputValues, (1, 5)),
+                CountTreesOnSlope(inputValues, (1, 7)),
+                CountTreesOnSlope(inputValues, (2, 1)),
+            };
+            long treeCountsProduct = treeCounts.Product();
+
+            PuzzleIOManager.SaveTextLines(
+                RelativeOutputPaths[1], new string[] { treeCountsProduct.ToString() });
         }
 
         public static void Run()
